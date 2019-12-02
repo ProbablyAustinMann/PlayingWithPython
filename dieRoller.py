5a5840e230bb2337c571d495d85771382629b168
Austin Taylor Mann <Fancysause@gmail.com>
2019-12-02 18:07:17 -0500

    more comments to explain how scripts work:

--- dieRoller.py
+++ dieRoller.py
@@ -1,6 +1,6 @@
 import random
 
-
+#this is a simple script that rolls two imaginary die
 def roll_die():
     x = random.randint(1, 6)
     y = random.randint(1, 6)
@@ -8,8 +8,10 @@ def roll_die():
 
 roll_count = 0
 roll_max = 0
+#user inputs imaginary money
 user_money = input("Enter money you'd like to waste\n")
 max_dollars = user_money
+#the script continues to run until you run out of money
 while user_money > 0:
     roll_die()
     roll_count += 1
@@ -23,6 +25,6 @@ while user_money > 0:
     if user_money > max_dollars:
         max_dollars = user_money
         roll_max = roll_count
-
+#after you go broke, you are told how many rolls you lasted and when you should have quit
 print("You went broke after %i rolls." % roll_count)
 print("You should have stopped at %i rolls when you had %i dollars." %(roll_max, max_dollars))
